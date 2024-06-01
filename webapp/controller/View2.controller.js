60e15069836742d98af33abc66111000b592d2ed
Himanshu Gupta <himanshu.gupta07@sap.com>
2024-06-01 16:07:32 +0000

    initial debugging

---
+++ webapp/controller/View2.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/Router"
+], function (Controller, Router) {
+    "use strict";
+    return Controller.extend("grcmom.controller.View2", {
+        onInit: function () {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.getRoute("RouteView2").attachPatternMatched(this._onObjectMatched, this);
+            console.log(oRouter)
+        },
+
+        _onObjectMatched: function (oEvent) {
+            var oParameters = oEvent.getParameter("arguments"); // Retrieve all parameters
+            console.log(oParameters)
+            var sValue = oParameters.value; // Retrieve the specific parameter value
+            console.log(sValue)
+            
+            // Ensure that the ID matches the ID of the UI element in your XML view
+            this.byId("label1").setText(sText); // Adjusted ID according to the XML view
+        }
+    });
+});
