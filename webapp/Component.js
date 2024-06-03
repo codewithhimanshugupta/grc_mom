dacf75a348bac96701dfc966b03615639a7c78c6
Himanshu Gupta <himanshu.gupta07@sap.com>
2024-06-03 10:17:59 +0000

    working

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,35 +1,36 @@
-/**
- * eslint-disable @sap/ui5-jsdocs/no-jsdoc
- */
-
 sap.ui.define([
-        "sap/ui/core/UIComponent",
-        "sap/ui/Device",
-        "grcmom/model/models"
-    ],
-    function (UIComponent, Device, models) {
-        "use strict";
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "grcmom/model/models",
+    "sap/ui/model/json/JSONModel"
+],
+function (UIComponent, Device, models, JSONModel) {
+    "use strict";
 
-        return UIComponent.extend("grcmom.Component", {
-            metadata: {
-                manifest: "json"
-            },
+    return UIComponent.extend("grcmom.Component", {
+        metadata: {
+            manifest: "json"
+        },
 
-            /**
-             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-             * @public
-             * @override
-             */
-            init: function () {
-                // call the base component's init function
-                UIComponent.prototype.init.apply(this, arguments);
+        /**
+         * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+         * @public
+         * @override
+         */
+        init: function () {
+            // call the base component's init function
+            UIComponent.prototype.init.apply(this, arguments);
 
-                // enable routing
-                this.getRouter().initialize();
+            // enable routing
+            this.getRouter().initialize();
 
-                // set the device model
-                this.setModel(models.createDeviceModel(), "device");
-            }
-        });
-    }
+            // set the device model
+            this.setModel(models.createDeviceModel(), "device");
+
+            // Create and set the global model
+            var oGlobalModel = new JSONModel();
+            this.setModel(oGlobalModel, "globalModel");
+        }
+    });
+}
 );
